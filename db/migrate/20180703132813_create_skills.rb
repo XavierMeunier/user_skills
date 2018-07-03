333b885427d1b52ab71829cfe2e48062a81c1f24
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-07-03 15:44:10 +0200

    fix(migrations):correct int to integer in migrations

--- db/migrate/20180703132813_create_skills.rb
+++ db/migrate/20180703132813_create_skills.rb
@@ -2,7 +2,7 @@ class CreateSkills < ActiveRecord::Migration[5.2]
   def change
     create_table :skills do |t|
       t.string :name
-      t.int :parent_id
+      t.integer :parent_id
 
       t.timestamps
     end
