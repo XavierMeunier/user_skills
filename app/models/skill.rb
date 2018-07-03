ccdda085642f8b27b7d6c0c05f9e180de38e28f0
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-07-03 16:41:41 +0200

    feat(skill):add link between children & parent on skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,10 @@
 class Skill < ApplicationRecord
 
+  # Skill link between parent & children
+  belongs_to              :parent,    class: 'Skill'
+  has_many                :children,  class: 'Skill', foreign_key: 'parent_id'
+
+
   has_and_belongs_to_many :users
 
 end
