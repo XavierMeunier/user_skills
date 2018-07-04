0dd71acd302f4830d152bdc06cb83a855962a241
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-07-04 17:03:34 +0200

    test(user):add tests to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # Currently limited to one skill in database
   has_and_belongs_to_many :skills
 
-  validates :points, :numericality => true
+  validates :points, presence: true, numericality: true
 
 end
