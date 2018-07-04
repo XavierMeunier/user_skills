93a5268003d8aed045525767158d744dbfbb3ec0
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-07-05 00:34:11 +0200

    style(user):design users' views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def user_skill_name(user)
+    user.user_skill.blank? ? "" : user.user_skill.name
+  end
+  
+  def user_skill_id(user)
+    user.user_skill.blank? ? nil : user.user_skill.id
+  end
+
 end
